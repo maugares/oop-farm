4a393586bb5ea9626a813a6b52e1c0091819d820
maugares <mauro.garciaesteban@gmail.com>
2019-03-27 13:43:01 +0100

    create parent class Crop and child classes Wheat and Sugarcane. In parent class define methods getYieldInEuros() and getCosts(). In childres class, define getYieldInKg() and define the price and cost of each crop.

---
+++ crops.js
@@ -0,0 +1,44 @@
+class Crop {
+  constructor(acres) {
+    this.acres = acres;
+  }
+
+  getYieldInEuros() {
+    return this.getYieldInKg() * this.price;
+  }
+
+  getCosts() {
+    return this.acres * this.cost;
+  }
+}
+
+class Wheat extends Crop {
+  constructor(acres) {
+    super(acres);
+    this.price = 1.5;   ///1.5€/kg
+    this.cost = 340;    // 340€/acre
+  }
+  
+  getYieldInKg() {
+    return Math.pow(this.acres * 1.5, 1.3);
+  }
+}
+
+class Sugarcane extends Crop {
+  constructor(acres) {
+    super(acres);
+    this.price = 2;     // 2€/kg
+    this.cost = 166;    // 166€/acre
+  }
+
+  getYieldInKg() {
+    return Math.pow(this.acres * 2.6, 1.1);
+  }
+}
+
+module.exports.Wheat = {Wheat};
+module.exports.Sugarcane = {Sugarcane};
+module.exports.Crop = {Crop};
+
+// 1 EXPORT
+// module.exports = { Wheat, Sugarcane, Crop};
